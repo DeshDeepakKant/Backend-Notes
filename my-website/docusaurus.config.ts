8d73e6f22285486b1e8ef9b723af93915b8b2dd6
DeshDeepakKant <deshdeepakkant@gmail.com>
2026-03-03 18:43:01 +0530

    Fix Docusaurus GitHub Pages trailingSlash config

--- my-website/docusaurus.config.ts
+++ my-website/docusaurus.config.ts
@@ -1,5 +1,5 @@
-import {themes as prismThemes} from 'prism-react-renderer';
-import type {Config} from '@docusaurus/types';
+import { themes as prismThemes } from 'prism-react-renderer';
+import type { Config } from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 // This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
@@ -19,6 +19,7 @@ const config: Config = {
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/Backend-Notes/',
+  trailingSlash: false,
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
@@ -83,8 +84,8 @@ const config: Config = {
           title: 'Docs',
           items: [
             {
-               label: 'Introduction',
-               to: '/docs/01_Roadmap_for_backend_from_first_principles',
+              label: 'Introduction',
+              to: '/docs/01_Roadmap_for_backend_from_first_principles',
             },
           ],
         },
